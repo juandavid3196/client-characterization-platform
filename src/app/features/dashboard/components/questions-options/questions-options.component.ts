12bc5231a84d7a39d0a79c82c9b2f9be7498722c
juan david rios <juandavid3196@gmail.com>
2025-01-07 09:02:37 -0500

    new fields in sectionForm  and validators working

--- src/app/features/dashboard/components/questions-options/questions-options.component.ts
+++ src/app/features/dashboard/components/questions-options/questions-options.component.ts
@@ -1,45 +1,54 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Section } from '../../models/section.model';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { v4 as uuidv4 } from 'uuid';
 import { DashboardlsService } from '../../services/dashboardls.service';
 
 @Component({
   selector: 'app-questions-options',
   templateUrl: './questions-options.component.html',
-  styleUrls: ['./questions-options.component.scss']
+  styleUrls: ['./questions-options.component.scss'],
 })
 export class QuestionsOptionsComponent {
-
-  close : boolean = false;
+  close: boolean = false;
   changeSection: boolean = false;
-  errorMessage : boolean =  false;
-  @Output() formClose = new EventEmitter<void>()
+  errorMessage: boolean = false;
+  @Output() formClose = new EventEmitter<void>();
   @Output() typeSelected = new EventEmitter<string>();
   @Output() sectionSelected = new EventEmitter<string>();
   @Output() openBank = new EventEmitter<void>();
   @Output() refreshList = new EventEmitter<void>();
-  @Output() closeEditWindow =  new EventEmitter<void>();
-  @Input() section !: Section;
-  @Input() editSection : boolean  =false;
-  
+  @Output() closeEditWindow = new EventEmitter<void>();
+  @Input() section!: Section;
+  @Input() editSection: boolean = false;
 
+  sectionForm!: FormGroup;
 
-  sectionForm !: FormGroup;
-
-  constructor( private fb:FormBuilder, private dashboardlsService: DashboardlsService){
+  constructor(
+    private fb: FormBuilder,
+    private dashboardlsService: DashboardlsService
+  ) {
     this.sectionForm = this.fb.group({
-      id:'',
-      title: ['',Validators.required],
-      type:'section',
-      icon:'section-icon'
-    })
+      id: '',
+      title: ['', Validators.required],
+      description: '',
+      video: ['', [this.validateIframeYoutube]],
+      imageUrl: ['', [this.validateUrl(/https?:\/\/[^\s$.?#].[^\s]*$/)]],
+      type: 'section',
+      icon: 'section-icon',
+    });
   }
 
-  ngOnInit():void {
-    if(this.editSection){
-     this.changeSection = !this.changeSection;
-     this.sectionForm.patchValue(this.section);
+  ngOnInit(): void {
+    if (this.editSection) {
+      this.changeSection = !this.changeSection;
+      this.sectionForm.patchValue(this.section);
     }
   }
 
@@ -54,22 +63,41 @@ export class QuestionsOptionsComponent {
     }
   }
 
-  onChangeSection():void {
-    if(this.editSection) {
+  // Validador para el iframe de YouTube
+  validateIframeYoutube(control: any) {
+    if (!control.value) return null;
+    const iframeRegex =
+      /<iframe.*src="https:\/\/www\.youtube\.com\/embed\/.*".*<\/iframe>/;
+    return iframeRegex.test(control.value) ? null : { invalidIframe: true };
+  }
+
+  // Validador personalizado
+  validateUrl(regex: RegExp) {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+        return null; // Campo vacío es válido
+      }
+      return regex.test(value) ? null : { invalidUrl: true }; // Aplica la validación solo si hay contenido
+    };
+  }
+
+  onChangeSection(): void {
+    if (this.editSection) {
       return;
     }
     this.changeSection = !this.changeSection;
   }
-  
-  onClose():void {
-    this.close =  true;
-   this.closeEditWindow.emit();
-    setTimeout(()=>{
+
+  onClose(): void {
+    this.close = true;
+    this.closeEditWindow.emit();
+    setTimeout(() => {
       this.formClose.emit();
-    },500);
+    }, 500);
   }
 
-  closeSection(event:Event) : void {
+  closeSection(event: Event): void {
     event.preventDefault();
     this.editSection = false;
     this.refreshList.emit();
@@ -77,37 +105,43 @@ export class QuestionsOptionsComponent {
   }
 
   onSubmit(): void {
+    console.log(this.sectionForm.value, this.sectionForm.valid);
     if (this.sectionForm.valid) {
-      
       if (this.editSection) {
         const storedQuestions = this.dashboardlsService.getDashboardOptions();
         if (storedQuestions && this.section.id) {
-          const index = storedQuestions.findIndex((e:any)  => e.id === this.section.id);
-      
+          const index = storedQuestions.findIndex(
+            (e: any) => e.id === this.section.id
+          );
+
           if (index !== -1) {
-            storedQuestions[index] = { ...storedQuestions[index], ...this.sectionForm.value };
+            storedQuestions[index] = {
+              ...storedQuestions[index],
+              ...this.sectionForm.value,
+            };
             this.dashboardlsService.saveDashboardOptions(storedQuestions);
             this.editSection = false;
+            console.log(this.sectionForm.value);
             this.refreshList.emit();
           }
-        }   
+        }
       } else {
         // Crear nueva sección
         this.sectionForm.patchValue({
-          id: uuidv4()
+          id: uuidv4(),
         });
-        this.sectionSelected.emit(this.sectionForm.value);    
+        console.log(this.sectionForm.value);
+        this.sectionSelected.emit(this.sectionForm.value);
       }
-    }else {
+    } else {
       this.errorMessage = !this.errorMessage;
       return;
     }
     this.onClose();
   }
 
-  sendInfo() : void {
+  sendInfo(): void {
     this.onClose();
-    this.openBank.emit()
+    this.openBank.emit();
   }
-
 }

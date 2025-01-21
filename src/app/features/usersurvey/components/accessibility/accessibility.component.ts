009d036c94ec2ffeef1d5288018ec7a62462d5cc
juan david rios <juandavid3196@gmail.com>
2025-01-21 16:40:24 -0500

    Accessibility comonent design and styling done.

--- src/app/features/usersurvey/components/accessibility/accessibility.component.ts
+++ src/app/features/usersurvey/components/accessibility/accessibility.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-accessibility',
   templateUrl: './accessibility.component.html',
-  styleUrls: ['./accessibility.component.scss']
+  styleUrls: ['./accessibility.component.scss'],
 })
 export class AccessibilityComponent {
+  isVisible: boolean = false;
 
+  togglePanel() {
+    this.isVisible = !this.isVisible;
+  }
 }
